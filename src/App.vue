262de982dfdab1b42461ea6675c67625d40f8775
Zero <339369805@qq.com>
2018-09-05 08:59:55 +0800

    Create App.vue

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <transition name="fade">
+      <keep-alive>
+        <router-view></router-view>
+      </keep-alive>
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data() {
+    return {
+
+    }
+  },
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
