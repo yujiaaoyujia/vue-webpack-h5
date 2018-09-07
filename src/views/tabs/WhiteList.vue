3152087fc8b3d228567feb06bca4906ac6a6ed7b
Zero <339369805@qq.com>
2018-09-07 16:58:48 +0800

    Add files via upload

---
+++ src/views/tabs/WhiteList.vue
@@ -0,0 +1,45 @@
+<template>
+  <article class="white-list">
+    WhiteList
+  </article>
+</template>
+
+<script>
+export default {
+  name: 'WhiteList',
+  data() {
+    return {
+
+    }
+  },
+
+  computed: {
+
+  },
+
+  created() {
+
+  },
+
+  mounted() {
+
+  },
+
+  activated() {
+
+  },
+
+  methods: {
+
+  },
+
+  components: {
+
+  },
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '../../assets/css/var.styl'
+
+</style>
