9e45482e40fd09c5bddc09f58435795da6c7f465
Zero <339369805@qq.com>
2018-09-07 16:56:45 +0800

    Add files via upload

---
+++ src/components/MainHead.vue
@@ -0,0 +1,74 @@
+<template>
+  <header class="main-head">
+    <!--<p class="head-logo"></p>-->
+    <div class="head-space"><slot></slot></div>
+    <label class="head-logout">注销</label>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'MainHead',
+  props: {
+
+  },
+
+  data() {
+    return {
+
+    }
+  },
+
+  computed: {
+
+  },
+
+  mounted() {
+
+  },
+
+  methods: {
+
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '../assets/css/var.styl'
+
+.main-head {
+  display: flex
+  align-items: center
+  padding: 0 26px
+  height: 56px
+  background: $theme
+  user-select: none
+}
+
+.head-logo {
+  display: block
+  width: 140px
+  height: 44px
+  /*background: url('../assets/img/logo.png') center center no-repeat*/
+  /*background-size: 100%*/
+}
+.head-space {
+  flex: 1
+}
+.head-logout {
+  display: block
+  line-height: 32px
+  font-size: 14px
+  color: $theme-weak
+  cursor: pointer
+  transition: all 200ms
+  &:hover {
+    color: $white
+  }
+  &:active {
+    color: $theme-weak
+    opacity: 0.6
+  }
+}
+
+</style>
