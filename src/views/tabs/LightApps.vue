c8cd71acb5894fc41d20ba0d626b6a28be205885
Zero <339369805@qq.com>
2018-09-07 16:58:31 +0800

    Add files via upload

---
+++ src/views/tabs/LightApps.vue
@@ -0,0 +1,45 @@
+<template>
+  <article class="light-apps">
+    LightApps
+  </article>
+</template>
+
+<script>
+export default {
+  name: 'LightApps',
+  data() {
+    return {
+
+    }
+  },
+
+  computed: {
+
+  },
+
+  created() {
+
+  },
+
+  mounted() {
+
+  },
+
+  activated() {
+
+  },
+
+  methods: {
+
+  },
+
+  components: {
+
+  },
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '../../assets/css/var.styl'
+
+</style>
