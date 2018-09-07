d19f475c12b66ddca0cb831346f523fcc5c44802
Zero <339369805@qq.com>
2018-09-07 16:58:10 +0800

    Add files via upload

---
+++ src/views/tabs/Base.vue
@@ -0,0 +1,45 @@
+<template>
+  <article class="base">
+    Base
+  </article>
+</template>
+
+<script>
+export default {
+  name: 'Base',
+  data() {
+    return {
+
+    }
+  },
+
+  computed: {
+
+  },
+
+  created() {
+
+  },
+
+  mounted() {
+
+  },
+
+  activated() {
+
+  },
+
+  methods: {
+
+  },
+
+  components: {
+
+  },
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '../../assets/css/var.styl'
+
+</style>
